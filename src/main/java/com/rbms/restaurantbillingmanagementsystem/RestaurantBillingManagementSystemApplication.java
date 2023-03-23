72786c62953a54876c21f1e34ce98d6e514b7118
ariful-limon <arifuldsi@gmail.com>
2023-03-24 00:54:53 +0600

    restaurant-billing-management-system initialized

---
+++ src/main/java/com/rbms/restaurantbillingmanagementsystem/RestaurantBillingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rbms.restaurantbillingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBillingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBillingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbms/restaurantbillingmanagementsystem/RestaurantBillingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbms.restaurantbillingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBillingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
